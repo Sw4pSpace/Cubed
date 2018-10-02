254127af7f75b19593371ed3a04feb9c17ddd91c
Jack DeSive <jackdesive17@gmail.com>
2018-10-01 22:07:43 -0500

    #130 Cannot create nether portals
    Fixed creation of nether portal. When player clicks with flint and steel on obsidian we will check if the clicked obsidian is part of a nether portal frame. If true we will fill the frame with nether portal blocks.

--- src/main/java/net/glowstone/block/itemtype/ItemFlintAndSteel.java
+++ src/main/java/net/glowstone/block/itemtype/ItemFlintAndSteel.java
@@ -13,6 +13,10 @@ import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
+import static org.bukkit.Material.*;
+import static org.bukkit.block.BlockFace.DOWN;
+import static org.bukkit.block.BlockFace.UP;
+
 public class ItemFlintAndSteel extends ItemTool {
 
     public ItemFlintAndSteel() {
@@ -22,9 +26,12 @@ public class ItemFlintAndSteel extends ItemTool {
     @Override
     public boolean onToolRightClick(GlowPlayer player, GlowBlock target, BlockFace face,
         ItemStack holding, Vector clickedLoc, EquipmentSlot hand) {
-        if (target.getType() == Material.OBSIDIAN) {
-            fireNetherPortal(target, face);
-            return true;
+        if (target.getType() == OBSIDIAN) {
+            BlockFace portalStartDirection = isNetherPortal(target);
+            if(portalStartDirection != null) {
+                createNetherPortal(target, portalStartDirection);
+                return true;
+            }
         }
         if (target.getType() == Material.TNT) {
             fireTnt(target, player);
@@ -37,16 +44,55 @@ public class ItemFlintAndSteel extends ItemTool {
         return false;
     }
 
-    private void fireNetherPortal(GlowBlock target, BlockFace face) {
-        if (face == BlockFace.UP || face == BlockFace.DOWN) {
-            target = target.getRelative(face);
-            int limit = 0;
-            while (target.getType() == Material.AIR && limit < 23) {
-                target.setType(Material.PORTAL);
-                target = target.getRelative(face);
-                limit++;
+    private BlockFace isNetherPortal(GlowBlock target) {
+        BlockFace[] ALL_DIRECTIONS = {BlockFace.SOUTH, BlockFace.NORTH, BlockFace.EAST, BlockFace.WEST};
+
+        GlowBlock oneUp = target.getRelative(UP);
+        if (!oneUp.getType().equals(AIR)) {
+            // This cannot be a portal
+            return null;
+        }
+
+        for (BlockFace f : ALL_DIRECTIONS) {
+            GlowBlock rel = oneUp.getRelative(f);
+            if (rel.getType().equals(OBSIDIAN)) {
+                // Hey look this might be a portal
+                if (rel.getRelative(UP).getType().equals(OBSIDIAN) && rel.getRelative(UP).getRelative(UP).getType().equals(OBSIDIAN)) {
+                    // We now have a wall
+                    GlowBlock wall = rel.getRelative(UP).getRelative(UP);
+                    if (wall.getRelative(f.getOppositeFace()).getRelative(UP).getType().equals(OBSIDIAN)) {
+                        // C shape, can we get another to make the O?
+                        if (wall.getRelative(f.getOppositeFace()).getRelative(f.getOppositeFace()).getRelative(UP).getType().equals(OBSIDIAN)) {
+                            // ceiling is complete
+                            GlowBlock ceil = wall.getRelative(f.getOppositeFace()).getRelative(f.getOppositeFace()).getRelative(UP);
+                            if (ceil.getRelative(DOWN).getRelative(f.getOppositeFace()).getType().equals(OBSIDIAN)) {
+                                GlowBlock wall2 = ceil.getRelative(DOWN).getRelative(f.getOppositeFace());
+                                if (wall2.getRelative(DOWN).getType().equals(OBSIDIAN) && wall2.getRelative(DOWN).getRelative(DOWN).getType().equals(OBSIDIAN)) {
+                                    // Wall 2 complete, this is looking to be a nether portal
+                                    if (target.getRelative(f.getOppositeFace()).getType().equals(OBSIDIAN)) {
+                                        // This is a nether portal! phew...
+                                        return f;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
+        return null;
+    }
+
+    private void createNetherPortal(GlowBlock target, BlockFace f) {
+        GlowBlock oneUp = target.getRelative(UP);
+        Material portalMat = PORTAL;
+
+        oneUp.setType(portalMat);
+        oneUp.getRelative(UP).setType(portalMat);
+        oneUp.getRelative(UP).getRelative(UP).setType(portalMat);
+        oneUp.getRelative(f.getOppositeFace()).setType(portalMat);
+        oneUp.getRelative(f.getOppositeFace()).getRelative(UP).setType(portalMat);
+        oneUp.getRelative(f.getOppositeFace()).getRelative(UP).getRelative(UP).setType(portalMat);
     }
 
     private void fireTnt(GlowBlock tnt,GlowPlayer player) {
